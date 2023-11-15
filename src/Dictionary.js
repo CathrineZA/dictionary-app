667cfbd7818c53a9f99e204272c23570bd115484
Makoma Cathrine Sekhuto <ccathy203@gmail.com>
2023-11-15 20:25:20 +0200

    added new components

---
+++ src/Dictionary.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+import Results from "./Results";
+
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState("sky");
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sky, wine, sunset, flower...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    loaded();
+    return "Loading";
+  }
+}
